359a6eedce0a68efd0260147a1ed26c7f0cadc09
avbabu025 <avbabu025@gmail.com.com>
2026-08-18 18:40:54 +0530

    variables ;added 2lGanapathi

--- variables.sh
+++ variables.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+Ganapathi1=maha
+Ganapathi2=babu
+
+
 echo "Babu:; Hi maha "
 echo "Maha:; Hello Babu ?"
+echo "Babu:; How are you, i hope you are doieng well"
+echo "Maha:; Thanks for asking me, yes i am good what about you?"
+echo "Babu:; thank you, i am also good...."
+
+echo "$Ganapathi2:; Hi $Ganapathi1 "
+echo "$Ganapathi1:; Hello $Ganapathi2 ?"
 echo "Babu:; How are you, i hope you are doieng well"
 echo "Maha:; Thanks for asking me, yes i am good what about you?"
 echo "Babu:; thank you, i am also good...."

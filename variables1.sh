cdfad420646a6bc41a2bcb4cc6e7666d3c7fe35f
avbabu025 <avbabu025@gmail.com.com>
2026-08-18 20:31:05 +0530

    user and passwd args pass,read, passwd&read-s ,printinguser&Passwd

--- variables1.sh
+++ variables1.sh
@@ -4,6 +4,10 @@ echo "please enter your username::"
 
 read -s USERNAME
 
+echo "print username:: $USERNAME"
+
 echo "Please enter your password::"
 
 read -s PASSWORD
+
+echo "Print Passwd:: $PASSWORD"

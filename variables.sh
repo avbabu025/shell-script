cc8a41121e1400fbb2eb018bc981b1a92f131e26
avbabu025 <avbabu025@gmail.com.com>
2026-08-18 18:30:36 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Babu: Hi maha "
+echo "Maha: Hello Babu ?"
+echo "Babu: How are you, i hope you are doieng well"
+echo "Maha: Thanks for asking me, yes i am good what about you?"
+echo "Babu: thank you, i am also good...."
